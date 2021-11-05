7815c117e2eec41b8be4181bb463f9879daf0aff
Russo Salvatore <russo.salvatore@osdbsesto>
2021-11-05 15:34:39 +0100

    add

---
+++ connessione/mysql3.php
@@ -0,0 +1,5 @@
+<?php
+function connDB($hn,$user,$pws,$schrma){
+    $conn=new mysqli($hn,$user,$pw,$scherma);
+}
+?>
--- connessione/mysqli2.php
+++ connessione/mysqli2.php
@@ -1,8 +1,15 @@
 <?php
+if(!empty($_POST) ){
  $servername = $_POST['servername'];
  $porta=$_POST['porta'];
  $username = $_POST['username'];
  $password = $_POST['password'];
  $nameDB = $_POST['nameDB'];
- $connessione=new mysqli($servername,$porta,$username,$password,$nameDB);
+ $connessione=new mysqli($servername,$username,$password,$nameDB);
+ if($conn->connect_error){
+     die("connessione fallita: ".$conn->connect_error)
+ }else{
+     echo "connessione riuscita";
+ }
+}
  ?>
