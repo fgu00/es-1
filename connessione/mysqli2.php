b46045f3f82e9b9499d67e18edaac39252fceb0f
Russo Salvatore <russo.salvatore@osdbsesto>
2021-11-05 15:15:44 +0100

    add

--- connessione/mysqli2.php
+++ connessione/mysqli2.php
@@ -1,7 +1,8 @@
 <?php
  $servername = $_POST['servername'];
+ $porta=$_POST['porta'];
  $username = $_POST['username'];
  $password = $_POST['password'];
  $nameDB = $_POST['nameDB'];
- $connessione=new mysqli($servername,$username,$password,$nameDB);
+ $connessione=new mysqli($servername,$porta,$username,$password,$nameDB);
  ?>
