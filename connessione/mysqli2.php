eef638deb0fbf7ba85b54e937ef8cf605e8b2c7b
Russo Salvatore <russo.salvatore@osdbsesto>
2021-11-05 16:10:28 +0100

    add

--- connessione/mysqli2.php
+++ connessione/mysqli2.php
@@ -1,15 +1,17 @@
 <?php
-if(!empty($_POST) ){
+//if(!empty($_POST) ){
  $servername = $_POST['servername'];
  $porta=$_POST['porta'];
  $username = $_POST['username'];
  $password = $_POST['password'];
  $nameDB = $_POST['nameDB'];
  $connessione=new mysqli($servername,$username,$password,$nameDB);
- if($conn->connect_error){
-     die("connessione fallita: ".$conn->connect_error)
+ if($connessione->connect_error){
+     die("connessione fallita: ".$connessione->connect_error);
  }else{
-     echo "connessione riuscita";
+     echo 'connessione riuscita';
  }
-}
+ $sql='INSERT INTO SCUOLA VALUES()  ';
+ $m=$connessione->query($sql);
+//}
  ?>
