12a2aa6553bcf589f420d23d58d90e190fff17cd
Russo Salvatore <russo.salvatore@osdbsesto>
2021-11-12 16:12:07 +0100

    add file

--- connessione/acesso_database.php
+++ connessione/acesso_database.php
@@ -4,27 +4,29 @@ servername:<input type="text" name="servername"></br>
 username:<input type="text" name="username"></br>
 password:<input type="password" name="password"></br>
 nameDB:<input type="text" name="nameDB"></br>
-<input type="submit" values="spedisci">
+<input type="submit" value="spedisci">
 </form>';
 
-     if(!empty($_POST)){
+    // if( !emply($_POST['servername'])&& !emply($_POST['username'])&&!emply($_POST['password'])&&!emply($_POST['nameDB'])){
          $host=$_POST['servername'];
          $user=$_POST['username'];
          $password=$_POST['password'];
          $name=$_POST['nameDB'];
          include "mysql3.php";
         $conn= connDB($host,$user,$password,$name);
-        if(!empty($conn)){
-            echo "efef";
-        }
-
-         if(connect_error($conn)){
-            die ("connessione fallita: ".connect_error($conn));
+         if($conn->connect_error && $name->empty){
+            die ("connessione fallita: ".$conn->connect_error);
          }else{
-             echo 'connessione riuscita';
+             echo "connessione riuscita";
+             $conn->close();
+             $file=fopen("D:\Utenti\russo.salvatore\Desktop\es-1\connessione\configurazione\configurazione.php");
+             $testo='<php
+             $db=array(
+                url=$ '
          }
+        
 
-        // echo  header('locate : http://russo.salvatore.tave.osdb.it/inizio_connessiome.html/');
-       //  header('locate : http://russo.salvatore.tave.osdb.it/inizio_connessiome.html/');
-     }
+         echo  header('locate : http://russo.salvatore.tave.osdb.it/inizio_connessiome.html/');
+        header('locate : http://russo.salvatore.tave.osdb.it/inizio_connessiome.html/');
+    // }
 ?>
---
+++ connessione/inserimento_dati.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <h1>invio dei dati </h1>
+    </head>
+    <body>
+        <form action="mysqli2.php" method="POST">
+            servername:<input type="text" name="servername"></br>
+            porta:<input type="text" name="porta"></br>
+            username:<input type="text" name="username"></br>
+            password:<input type="text" name="password"></br>
+            nameDB:<input type="text" name="nameDB"></br>
+            <input type="submit" values="spedisci">
+        </form>
+    </body>
+</html>
+
+<?php
+
+?>
