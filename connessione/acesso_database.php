4582f65fec426f8ee5cff506b1be86a239a05ecc
Russo Salvatore <russo.salvatore@osdbsesto>
2021-11-19 16:13:25 +0100

    add

--- connessione/acesso_database.php
+++ connessione/acesso_database.php
@@ -1,4 +1,5 @@
 <?php
+include "connDB.php";
 echo '<form action="acesso_database.php" method="POST">
 servername:<input type="text" name="servername"></br>
 username:<input type="text" name="username"></br>
@@ -6,17 +7,23 @@ password:<input type="password" name="password"></br>
 nameDB:<input type="text" name="nameDB"></br>
 <input type="submit" value="spedisci">
 </form>';
-
+if(!empty($_POST)){
          $host=$_POST['servername'];
+         echo $host;
          $user=$_POST['username'];
+         echo $user;
          $password=$_POST['password'];
          $name=$_POST['nameDB'];
+         echo $name;
          include "mysql3.php";
         $conn= connDB($host,$user,$password,$name);
-         if($conn->connect_error && $name->empty){
-            die ("connessione fallita: ".$conn->connect_error);
+        echo "      qui";
+         if(connect_error($conn)|| $con->connect_error){
+            die ("connessione fallita: ".connect_error($conn));
+           echo "connessione fallita";
          }else{
              echo "connessione riuscita";
+             echo "cioa";
              $conn->close();
              $file=fopen(__DIR__."D:\Utenti\russo.salvatore\Desktop\es-1\connessione\configurazione\configurazione.php","w");
             $testo='<?php
@@ -27,8 +34,10 @@ nameDB:<input type="text" name="nameDB"></br>
          );
          define("BASE_URL","'.dirname($_SERVER['HTTP_REFERER']).'/");
          ?>'; 
+         echo "<br>fatto</br>";
             $file.fwrite($file,$testo);
              $file.close($file);
              header('locate : '.dirname($_SERVER['HTTP_REFERER'])."/insert.php");      
          }
+        }
 ?>
--- connessione/connDB.php
+++ connessione/connDB.php
@@ -1,5 +1,6 @@
 <?php
 function connDB($hn,$user,$pws,$schrma){
+    echo "esiste";
     $conn=new mysqli($hn,$user,$pws,$scherma);
     return $conn;
 }
