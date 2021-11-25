469adbae419aef4728640bda46523fe18b597760
Russo Salvatore <russo.salvatore@osdbsesto>
2021-11-25 13:02:39 +0100

    add es file

---
+++ controllo_file/controllo_file.php
@@ -0,0 +1,18 @@
+<?php
+echo '<form action="controllo_file.php" method="POST">
+path<input type="text" name="input"></br>
+nome del file<input type="text" name="nfile"></br>
+<input type="submit" values="invio">
+</form>';
+if(!emply($_POST['input'])){
+$path=$_POST['input'];
+$nfile=$_POST['nfile'];
+$completo=$path."/".$nfile;
+if(file_exists($completo)){
+    echo "file trovato";
+}else{
+    echo "file inesistente";
+    $file=fopen($completo,"w");
+}
+}
+?>
