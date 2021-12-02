2496762c38e21ec65cf7e234be5fa79f0372d93d
Russo Salvatore <russo.salvatore@osdbsesto>
2021-12-02 13:12:53 +0100

    add

---
+++ controllo_file2/controllo_file2.php
@@ -0,0 +1,18 @@
+<?php
+    //__DIR__ contiene il path al file corrente.
+    echo '<form action="controllo_file2.php" method=POST>
+    path<input type="text" name="path"><br>
+    <input type="submit" values=invio>
+    </form>';
+    if(!empty($_POST["path"])){
+    $path=$_POST["path"];
+    echo "ROOT: $path</br>";
+    $contenuto = opendir($path);
+    while (($file = readdir($contenuto)) !== false) {
+            echo "filetype: ".filetype($path."/".$file)."   ".$file."</br>";
+        }
+        //echo "$file</br>";
+    }
+    closedir($contenuto);
+
+?>
