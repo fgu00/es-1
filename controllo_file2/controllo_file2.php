db452307410a0cabfd401e51130f1b0e4dad555b
Russo Salvatore <russo.salvatore@osdbsesto>
2021-12-03 16:14:11 +0100

    add

--- controllo_file2/controllo_file2.php
+++ controllo_file2/controllo_file2.php
@@ -1,17 +1,20 @@
 <?php
     //__DIR__ contiene il path al file corrente.
-    echo '<form action="controllo_file2.php" method=POST>
-     path<input type="text" name="path"><br>
-     <input type="submit" values=invio>
-     </form>';
+     echo "<table align=center>
+     <tr><th><t1>benvenuto</t1></th></tr>
+     <tr><th><form action= file2.php method=post></th></tr>
+     <tr><th>dimmi un path: <input type = text name=path></th></tr>
+     <tr><th><input type=submit value=submit> </th></tr>
+     </table>
+     </form>";
     if(!empty($_POST["path"])){
-    $path=$_POST["path"];
-    echo "ROOT: $path</br>";
-    $contenuto = opendir($path);
-    while (($file = readdir($contenuto)) !== false) {
-            echo "filetype: ".filetype($path."/".$file)." ---->  ".$file."</br>";
-        }
-        echo "$file</br>";
+    $contenuto[]=scandir($_POST["path"]);
+    echo "<table align=center>";
+    if($i=0;$i<count(scandir($_POST['path'])); $i++){
+        echo "<tr><th>DIR <a href=ardizzone.pietro.tave.osdb.it/".$_POST['path']."/".scandir($_POST['path'])[$i].">".scandir($_POST['path'])[$i]."</a></th></tr>";  
+    }else{
+        echo "<tr><th><a href=".$_POST['path']."/".scandir($_POST['path'])[$i].">".scandir($_POST['path'])[$i]."</a></th></tr>";
+    }
    }
     closedir($contenuto);
 
--- controllo_file2/controllo_file3.php
+++ controllo_file2/controllo_file3.php
@@ -3,14 +3,26 @@ $path=__DIR__."es";
 echo "ROOT: $path</br>";
 $contenuto = opendir($path);
 while (($file = readdir($contenuto)) !== false) {
-        echo "filetype: ".filetype($path."/".$file)." ---->  ".$file."</br>";
+        echo "filetype: ".filetype($path."/".$file)." ---->  "."<a href =$path.'/'.$file> qui </a>"."</br>";
     }
     echo "$file</br>";
-$dir=array();
-    while (($file = readdir($contenuto)) !== false) {
-       
-    }   
-}
+//$dir=array();
+//$file2=array();
+//foreach($n=0 as scandir($path)){
+  //  if(filetype(($path."/".$file)=="dir"){
+    //    $dir=array($n,scandir($path,$n));
+   // }else{
+//$file2=array($n,scandir($path,$n))
+  //  }
+//}
+//foreach($n=0 as $dir){
+  //  echo $dir."</br>";
+//}
+//foreach($n=0 as $file2){
+  //  echo $file2."</br>";
+//}
+
+//}
 closedir($contenuto);
 
 ?>
