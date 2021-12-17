f6987b6c53a3706d4aa378b3fa4acb7f01503ef9
Russo Salvatore <russo.salvatore@osdbsesto>
2021-12-17 16:11:56 +0100

    add

--- connesioneDQL/conn.php
+++ connesioneDQL/conn.php
@@ -1,10 +1,21 @@
 <?php
-$sql="SELECT * from `c190_primo`.Prodotti where prezzo>;
-$_POST['prezzo']...";
-$risultato=$conn->querry($sql);
-echo $sql;
-while($row=$risultato->fetch_assoc()){
-    echo $row;
+echo "<html>
+<head>
+    <title>Connect to DBMS</title>
+</head>
+<body>
+    <form action='mysql_connect.php' method='post'>
+    prezzo:<input type='text' name='prezzo'></br>
+        <input type='submit' values='spedisci'>
+    </body>
+    </html>";
     
-}
+$sql="SELECT * from `c190_primo`.Prodotti where prezzo>
+$_POST['prezzo']...";
+//$risultato=$conn->querry($sql);
+echo $sql;
+//while($row=$risultato->fetch_assoc()){
+  //  echo $row;
+
+//}
 ?>
