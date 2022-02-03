6e9e70c9e7b4c7713c65de9c15f4b23c8468a3be
Russo Salvatore <russo.salvatore@osdbsesto>
2022-02-03 13:06:35 +0100

    coorect es

--- es_coockie/index.php
+++ es_coockie/index.php
@@ -3,25 +3,66 @@ echo "<form method='POST'>
 inserisci un numero <input type='number' name='numero'>
 <input type='submit'>
 </form>";
-$valore=$_COOKIE['login'];
-echo $valore;
-setcookie("numero",$_POST["numero"]."+".$valore);
-$tot= $_POST["numero"].$valore; 
-$array=explode("+",$tot);
-$conto=count ($array);
-for($n=0;$n<$conto;$n++){
-    $totale=$array[$n]+$totale;
+if(!empty($_POST)){
+if($_COOKIE['conto']!=0){
+$numero=$_COOKIE['conto'];
+}else{
+  $numero=0;  
 }
-$medio=$totale/$conto;
+setcookie("$numero",$_POST['numero']);
+$numero++;
+setcookie("conto",$numero);
+if($numero==1){
+$max=$_POST['numero'];
+$min=$_POST['numero'];
+echo $_POST['numero'];
+echo $min;
+echo "gg";
+}else{
+    $max=$_COOKIE['max'];
+    $min=$_COOKIE['min'];   
+    echo "ii";
+}
+for($n=0;$n<$numero;$n++){
+    echo $n."</br>";
+    $val=$_COOKIE["$n"];
+    $tot=$tot+$val;
+    if($val>$max){
+       $max=$val;
+       setcookie("max",$max);
+    }
+    if($min>$val){
+        $min=$val;
+        setcookie("min",$min);
+    }
 
-echo "totale ".$totale."</br>";
-echo "media ".$medio;
+}
+if($_POST['numero']>$max){
+    $max=$val;
+    setcookie("max",$max);
+ }
+ if($min>$_POST['numero']){
+     $min=$val;
+     setcookie("min",$min);
+ }
+$medio=$tot/$numero;
+
+echo "totale ".$tot."</br>";
+echo "media ".$medio."</br>";
+echo "minimo ".$min."</br>";
+echo "massimo ".$max."</br>";
 
 echo " <form method='POST'>
 <input type='submit' name='pulsante'>
 </form>";
 if($_POST['pulsante']==true){
-    setcookie("numero");
+    echo $n."</br>";
+    for($n=0;$n<$numero-1;$n++){
+        setcookie("$n");  
+    }
+    setcookie("conto");
+    setcookie("max");
+    setcookie("min");
 }
-
+}
 ?>
